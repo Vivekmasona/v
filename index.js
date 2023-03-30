0ed6184cec7d4f46ca628ad7008afecd1361e0f7
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-03-30 12:25:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.get("/download", async (req, res) => {
   const itag = req.query.itag;
   const type = req.query.type;
 
-  // const info = await ytdl.getInfo(url);
-  // const title = info.videoDetails.title;
+  const info = await ytdl.getInfo(url);
+  const title = info.videoDetails.title;
 
-  res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
+  // res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
   try {
     ytdl(url, { itag }).pipe(res);
   } catch (err) {
