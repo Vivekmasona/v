485d15a43492b098d54e2cf17ede70ad4ae3b46e
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-03-30 21:05:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ app.get("/download", async (req, res) => {
 
 app.get("/video", async (req, res) => {
   const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
+  // const itag = req.query.itag;
+  // const type = req.query.type;
 
   const info = await ytdl.getInfo(url);
   const title = info.videoDetails.title;
